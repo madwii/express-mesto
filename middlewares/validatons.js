74abd4de8c4f6320aa6f906cd5f8c8e0af5ada24
madwii <wii7@yandex.ru>
2021-04-07 00:38:45 +0300

    codefix: err-fixed 2

--- middlewares/validatons.js
+++ middlewares/validatons.js
@@ -22,7 +22,7 @@ const validateCreateUser = celebrate({
       }
       return helpers.message('Невалидный email');
     }),
-    password: Joi.string().required().min(8).max(30),
+    password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().custom((value, helpers) => {
